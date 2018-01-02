c35af1fbc10ae4f396d13310d809883642079bb3
Varun <vrao@gogoair.com>
2018-01-02 01:29:28 -0500

    Write x,y coordinates to CSV File, update gitignore to ignore CSV File

--- retrieve_video_file.py
+++ retrieve_video_file.py
@@ -3,6 +3,17 @@ import imutils
 import cv2
 import csv
 
+
+
+class Point:
+    def __init__(self, x_coordinate=None, y_coordinate=None, time=None, frame_number=None):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        self.time = time
+        self.frame_number = frame_number
+
+
+
 #THRESHOLD
 #if the mice is between these two thresholds it will mask it to white
 def retrieve_info_from_video():
@@ -21,15 +32,15 @@ def retrieve_info_from_video():
     while True:
         #grabbed if the next frame was recieved or not. Frame will contain the next frame
         (grabbed, frame) = video.read()
+        time = int(video.get(cv2.CAP_PROP_POS_MSEC))
+        print("time is {}".format(time))
         #if the frame was not grabbed
         if not grabbed:
-            break
-         
-        #cv2.GaussianBlur(frame, (11, 11), 0)
-        mask = cv2.inRange(frame, miceLower, miceUpper)
-        #cv2.erode(mask, None, iterations=2)
-        #cv2.dilate(mask, None, iterations=2)
-
+            break 
+        #frame = cv2.GaussianBlur(frame, (11, 11), 0)
+        #frame = cv2.erode(frame, None, iterations=2)
+        #frame = cv2.dilate(frame, None, iterations=2)
+        mask =  cv2.inRange(frame, miceLower, miceUpper)
         params = cv2.SimpleBlobDetector_Params()
         params.filterByConvexity = True
         params.minConvexity = 0.4
@@ -38,25 +49,35 @@ def retrieve_info_from_video():
         params.filterByArea = True
         params.minArea = 300
         detector = cv2.SimpleBlobDetector_create(params)
-        key_points = detector.detect(255-mask)
+        key_points = detector.detect(mask)
+        
         image_with_keypoints = cv2.drawKeypoints(mask, key_points, numpy.array([]))
+        cv2.imshow('image_with_keypoints', image_with_keypoints)        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
         key_points_large.append(key_points)
-    create_or_edit_csv_file(key_points)
+    create_or_edit_csv_file(key_points_large)
 
 
  
             
 def create_or_edit_csv_file(key_points):
-    with open ('key_points.csv', 'r+') as csv_file:
+    with open ('key_points2.csv', 'w+') as csv_file:
         file_stream_writer = csv.writer(csv_file)
-        abc = csv.writer(csv_file)
+        print("key_points is {}".format(key_points))
+        print(type(key_points))
         for key_point in key_points:
-            abc.writeRow(key_point)
+            for x_and_y in key_point:
+                file_stream_writer.writerow(["{} {}".format(x_and_y.pt[0], x_and_y.pt[1])])
 
 
 
 
+         
+a = Point()
+a.x_coordinate = 5
+a.y_coordinate = 10
 
 
 if __name__ == '__main__':
-    retrieve_info_from_video()
+    retrieve_info_from_video()    
